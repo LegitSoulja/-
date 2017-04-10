b26dfeadbe465e922353e830a32689bbb174cfbc
ᦖєgιт <support@legitsoulja.info>
2017-04-10 05:27:53 -0400

    DON'T USE UNTIL FINAL RELEASE

--- parser.php
+++ parser.php
@@ -1,150 +1,117 @@
 <?php
 namespace SlickInject\Parser;
 
-class WHERE
-{
-    private static $object;
-    private static $index = 0;
-    function __construct($a)
-    {
-        self::$object = $a;
-    }
-    public function __toString()
-    {
-        $z = "";
-        foreach (self::$object as $a => $b) {
-            $z .= $this->format($a, mysql_escape_string($b), count(self::$object));
-            self::$index++;
-        }
-        return $z;
-    }
-    private function format($a, $b, $c)
-    {
-        if (self::$index > $c)
-            return;
-        if ($c == 1)
-            return "WHERE " . $a . "='" . $b . "'";
-        if (self::$index < 1) {
-            return "WHERE " . $a . "='" . $b . "' AND ";
-        } else if (self::$index === $c - 1) {
-            return "" . $a . "='" . $b . "'";
-        } else {
-            return "" . $a . "='" . $b . "' AND ";
-        }
-    }
+class WHERE{
+	private static $Vh0i2usftpgx;
+	private static $Vhmkrapki2ag = 0;
+	function __construct($Vjzr4bmfp21t){
+		self::$Vh0i2usftpgx = $Vjzr4bmfp21t;
+	}
+	public function __toString(){
+		$Vrg4wxvnhgwu = "";
+		foreach(self::$Vh0i2usftpgx as $Vjzr4bmfp21t => $Vnh3ttwycfw1){
+			$Vrg4wxvnhgwu .= $this->format($Vjzr4bmfp21t,mysql_escape_string($Vnh3ttwycfw1),count(self::$Vh0i2usftpgx));
+			self::$Vhmkrapki2ag++;
+		}
+		return $Vrg4wxvnhgwu;
+	}
+	private function format($Vjzr4bmfp21t,$Vnh3ttwycfw1,$Vp0f4eck25d0){
+		if(self::$Vhmkrapki2ag > $Vp0f4eck25d0) return;
+		if($Vp0f4eck25d0 == 1) return "WHERE ".$Vjzr4bmfp21t."='".$Vnh3ttwycfw1."'";
+		if(self::$Vhmkrapki2ag < 1){
+		  return "WHERE ".$Vjzr4bmfp21t."='".$Vnh3ttwycfw1."' AND ";
+		}else if(self::$Vhmkrapki2ag === $Vp0f4eck25d0 - 1){
+		  return "".$Vjzr4bmfp21t."='".$Vnh3ttwycfw1."'";
+		}else{
+		  return "".$Vjzr4bmfp21t."='".$Vnh3ttwycfw1."' AND ";
+		}
+	}
 }
-class INSERT
-{
-    private static $index = 0;
-    private static $object;
-    private static $table;
-    public function __construct($table, $object)
-    {
-        self::$object = $object;
-        self::$table  = $table;
+class INSERT{
+  private static $Vhmkrapki2ag = 0;
+  private static $Vh0i2usftpgx;
+  private static $Ve3cbuz1pb1n;
+  public function __construct($Ve3cbuz1pb1n, $Vh0i2usftpgx){
+    self::$Vh0i2usftpgx = $Vh0i2usftpgx;
+    self::$Ve3cbuz1pb1n = $Ve3cbuz1pb1n;
+  }
+  public function __toString(){
+    $V1sg4tggdlqs = "";
+    $Vgwuvjmtjyqr = "";
+    $Vp0f4eck25d0 = count(self::$Vh0i2usftpgx);
+    $Vv221xri1hvv = array();
+    foreach(self::$Vh0i2usftpgx as $Vjzr4bmfp21t => $Vnh3ttwycfw1){
+      if(in_array($Vjzr4bmfp21t, $Vv221xri1hvv)) continue;
+      array_push($Vv221xri1hvv, $Vjzr4bmfp21t);
+      $Vdu55u30j113 = $this->next($Vjzr4bmfp21t,mysql_escape_string($Vnh3ttwycfw1),$Vp0f4eck25d0);
+      $V1sg4tggdlqs .= $Vdu55u30j113["a"];
+      $Vgwuvjmtjyqr .= $Vdu55u30j113["b"];
+      self::$Vhmkrapki2ag++;
     }
-    public function __toString()
-    {
-        $keys   = "";
-        $values = "";
-        $c      = count(self::$object);
-        $dupes  = array();
-        foreach (self::$object as $a => $b) {
-            if (in_array($a, $dupes))
-                continue;
-            array_push($dupes, $a);
-            $r = $this->next($a, mysql_escape_string($b), $c);
-            $keys .= $r["a"];
-            $values .= $r["b"];
-            self::$index++;
-        }
-        return "INSERT INTO `" . self::$table . "` (" . $keys . ") VALUES (" . $values . ")";
+    return "INSERT INTO `".self::$Ve3cbuz1pb1n."` (".$V1sg4tggdlqs.") VALUES (".$Vgwuvjmtjyqr.")";
+  }
+  private function next($Vjzr4bmfp21t,$Vnh3ttwycfw1,$Vp0f4eck25d0){
+    if($Vp0f4eck25d0 == 1) return array("a"=>$Vjzr4bmfp21t,"b"=>"'".$Vnh3ttwycfw1."'");
+    if(self::$Vhmkrapki2ag == $Vp0f4eck25d0 - 1){
+      return array("a"=>$Vjzr4bmfp21t,"b"=>"'".$Vnh3ttwycfw1."'");
+    }else{
+      return array("a"=>$Vjzr4bmfp21t.",","b"=>"'".$Vnh3ttwycfw1."',");
     }
-    private function next($a, $b, $c)
-    {
-        if ($c == 1)
-            return array(
-                "a" => $a,
-                "b" => "'" . $b . "'"
-            );
-        if (self::$index == $c - 1) {
-            return array(
-                "a" => $a,
-                "b" => "'" . $b . "'"
-            );
-        } else {
-            return array(
-                "a" => $a . ",",
-                "b" => "'" . $b . "',"
-            );
-        }
-    }
+  }
 }
-class UPDATE
-{
-    private static $index = 0;
-    private static $object;
-    private static $where;
-    private static $table;
-    public function __construct($table, $object, $where)
-    {
-        self::$object = $object;
-        self::$table  = $table;
-        self::$where  = new WHERE($where);
+class UPDATE{
+  private static $Vhmkrapki2ag = 0;
+  private static $Vh0i2usftpgx;
+  private static $Vuj13qz2gnvo;
+  private static $Ve3cbuz1pb1n;
+  public function __construct($Ve3cbuz1pb1n, $Vh0i2usftpgx, $Vuj13qz2gnvo){
+    self::$Vh0i2usftpgx = $Vh0i2usftpgx;
+    self::$Ve3cbuz1pb1n = $Ve3cbuz1pb1n;
+    self::$Vuj13qz2gnvo = new WHERE($Vuj13qz2gnvo);
+  }
+  public function __toString(){
+    $Vxfd5zl34rwk = "UPDATE `".self::$Ve3cbuz1pb1n."` SET ";
+    $Vp0f4eck25d0 = count(self::$Vh0i2usftpgx);
+    foreach(self::$Vh0i2usftpgx as $Vjzr4bmfp21t => $Vnh3ttwycfw1){
+      $Vxfd5zl34rwk .= $this->next($Vjzr4bmfp21t,mysql_escape_string($Vnh3ttwycfw1),$Vp0f4eck25d0);
+      self::$Vhmkrapki2ag++;
     }
-    public function __toString()
-    {
-        $select = "UPDATE `" . self::$table . "` SET ";
-        $c      = count(self::$object);
-        foreach (self::$object as $a => $b) {
-            $select .= $this->next($a, mysql_escape_string($b), $c);
-            self::$index++;
-        }
-        $select .= " " . self::$where;
-        return $select;
+    $Vxfd5zl34rwk .= " ".self::$Vuj13qz2gnvo;
+    return $Vxfd5zl34rwk;
+  }
+  private static function next($Vjzr4bmfp21t,$Vnh3ttwycfw1,$Vp0f4eck25d0){
+    if($Vp0f4eck25d0 == 1) return $Vjzr4bmfp21t."='".$Vnh3ttwycfw1."'";
+    if(self::$Vhmkrapki2ag == $Vp0f4eck25d0 - 1){
+      return $Vjzr4bmfp21t."='".$Vnh3ttwycfw1."'";
+    }else{
+      return $Vjzr4bmfp21t."='".$Vnh3ttwycfw1."',";
     }
-    private static function next($a, $b, $c)
-    {
-        if ($c == 1)
-            return $a . "='" . $b . "'";
-        if (self::$index == $c - 1) {
-            return $a . "='" . $b . "'";
-        } else {
-            return $a . "='" . $b . "',";
-        }
-    }
+  }
 }
-class SELECT
-{
-    private static $table;
-    private static $columns;
-    private static $where;
-    private static $index;
-    public function __construct($table, $columns = null, $where = null)
-    {
-        if (!is_array($columns))
-            throw new \Exception("Args index 1 is not an array.");
-        self::$table   = $table;
-        self::$columns = (empty($columns)) ? "*" : join(",", $columns);
-        self::$where   = (empty($where)) ? null : new WHERE($where);
-    }
-    public function __toString()
-    {
-        return "SELECT " . (self::$columns) . " FROM `" . (self::$table) . "` " . (self::$where);
-    }
+class SELECT{
+	private static $Ve3cbuz1pb1n;
+	private static $Vp0f4eck25d0olumns;
+	private static $Vuj13qz2gnvo;
+	private static $Vhmkrapki2ag;
+	public function __construct($Ve3cbuz1pb1n, $Vp0f4eck25d0olumns = null, $Vuj13qz2gnvo = null){
+		if(!is_array($Vp0f4eck25d0olumns)) throw new \Exception("Args index 1 is not an array.");
+		self::$Ve3cbuz1pb1n = $Ve3cbuz1pb1n;
+		self::$Vp0f4eck25d0olumns = (empty($Vp0f4eck25d0olumns))?"*":join(",",$Vp0f4eck25d0olumns);
+		self::$Vuj13qz2gnvo = (empty($Vuj13qz2gnvo))?null:new WHERE($Vuj13qz2gnvo);
+	}
+	public function __toString(){
+		return "SELECT ".(self::$Vp0f4eck25d0olumns)." FROM `".(self::$Ve3cbuz1pb1n). "` ".(self::$Vuj13qz2gnvo);
+	}
 }
-class DELETE
-{
-    private static $object;
-    private static $table;
-    public function __construct($table, $object)
-    {
-        self::$object = $object;
-        self::$table  = $table;
-    }
-    public function __toString()
-    {
-        $where = new WHERE(self::$object);
-        return "DELETE FROM " . self::$table . " " . $where;
-    }
+class DELETE{
+  private static $Vh0i2usftpgx;
+  private static $Ve3cbuz1pb1n;
+  public function __construct($Ve3cbuz1pb1n, $Vh0i2usftpgx){
+    self::$Vh0i2usftpgx = $Vh0i2usftpgx;
+    self::$Ve3cbuz1pb1n = $Ve3cbuz1pb1n;
+  }
+  public function __toString(){
+    $Vuj13qz2gnvo = new WHERE(self::$Vh0i2usftpgx);
+    return "DELETE FROM ".self::$Ve3cbuz1pb1n." ".$Vuj13qz2gnvo;
+  }
 }
