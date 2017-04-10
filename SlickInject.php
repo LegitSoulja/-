d74447cfee8455696889cf71cd1e8041dc9e3580
ᦖєgιт <support@legitsoulja.info>
2017-04-10 05:17:03 -0400

    Update SlickInject.php

--- SlickInject.php
+++ SlickInject.php
@@ -5,17 +5,22 @@ include_once 'parser.php';
 
 use SlickInject\Parser as Parser;
 
-class SlickInject{
-	public static function INSERT($table,$object){
-		return new Parser\INSERT($table,$object);
-	}
-	public static function DELETE($table,$object){
-		return new Parser\DELETE($table,$object);
-	}
-	public static function SELECT($table,$c=null, $where=null){
-		return new Parser\SELECT($table,$c,$where);
-	}
-	public static function UPDATE($table,$object,$where){
-		return new Parser\UPDATE($table,$object,$where);
-	}
+class SlickInject
+{
+    public static function INSERT($table, $object)
+    {
+        return new Parser\INSERT($table, $object);
+    }
+    public static function DELETE($table, $object)
+    {
+        return new Parser\DELETE($table, $object);
+    }
+    public static function SELECT($table, $c = null,$where=null)
+    {
+        return new Parser\SELECT($table, $c, $where);
+    }
+    public static function UPDATE($table, $object, $where)
+    {
+        return new Parser\UPDATE($table, $object, $where);
+    }
 }
