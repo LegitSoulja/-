879494f24f9429f1c9229be1f02823519b9717eb
ᦖєgιт <support@legitsoulja.info>
2017-04-10 21:15:51 -0400

    Update SlickInject.php

--- SlickInject.php
+++ SlickInject.php
@@ -1,26 +1,52 @@
 <?
 namespace SlickInject;
 
-include_once 'parser.php';
-
 use SlickInject\Parser as Parser;
+use SlickInject\SQLObject as SQLObject;
 
 class SlickInject
 {
-    public static function INSERT($table, $object)
+    protected static $SQLObject;
+	protected static $parser;
+    function connect($dbhost, $dbuser, $dbpass, $dbname)
     {
-        return new Parser\INSERT($table, $object);
+        self::$SQLObject = new SQLObject();
+        return self::$SQLObject->connect($dbhost, $dbuser, $dbpass, $dbname);
     }
-    public static function DELETE($table, $object)
+    function isConnected()
     {
-        return new Parser\DELETE($table, $object);
+		return (gettype(self::$SQLObject) != null)?true:false;
     }
-    public static function SELECT($table, $c = null,$where=null)
+    static function INSERT($table, $object)
     {
-        return new Parser\SELECT($table, $c, $where);
+        if (!self::isConnected())
+            return new Parser\INSERT($table, $object);
+        else
+            return self::$SQLObject->query(new Parser\INSERT($table, $object,self::$SQLObject));
     }
-    public static function UPDATE($table, $object, $where)
+    static function DELETE($table, $object)
     {
-        return new Parser\UPDATE($table, $object, $where);
+        if (!self::isConnected())
+            return new Parser\DELETE($table, $object);
+        else
+            return self::$SQLObject->query(new Parser\DELETE($table, $object));
     }
+    static function SELECT($table, $c = null, $where = null)
+    {
+        if (!self::isConnected())
+            return new Parser\SELECT($table, $c, $where);
+        else
+            return self::$SQLObject->query(new Parser\SELECT($table, $c, $where,self::$SQLObject));
+    }
+    static function UPDATE($table, $object, $where)
+    {
+        if (!self::isConnected())
+            return new Parser\UPDATE($table, $object, $where);
+        else
+            return self::$SQLObject->query(new Parser\UPDATE($table, $object, $where,self::$SQLObject));
+    }
+	function close(){
+		if (!self::isConnected()) return;
+		return self::$SQLObject->close();
+	}
 }
