9ca0656306a36c9d84907f4513d3f3057754e43a
ᦖєgιт <support@legitsoulja.info>
2017-04-10 23:33:58 -0400

    Update test.php

--- test.php
+++ test.php
@@ -9,29 +9,40 @@ function _load_all($a)
         } else {
             if (basename($b, ".php") == basename(__FILE__, ".php"))
                 continue;
+
             if (pathinfo($b)['extension'] == "php") 
                 include $b;
         }
     }
 }
-_load_all("/lib/*");
+_load_all("lib/*");
 
 use SlickInject\SlickInject as SlickInject;
 
-echo SlickInject::SELECT("table", ["*"], array(
-    "id" => 1 // WHERE id=1
-)); // returns SQL string
+// usage before class initialization
+echo "<br/><br/>- <b>SQL (String)</b><br/>";
+print_r(SlickInject::SELECT("table",["*"]));
 
 // connect to database, and get row data
+
 $si = new SlickInject();
 $si->connect("localhost", "username", "password", "database");
-$si->SELECT("table", ["*", array(
+
+echo "<br/><br/>- <b>Table data (Array)</b><br/>";
+print_r($si->SELECT("table", ["*"], array(
     "id" => 1 // WHERE id=1
-))->returnRows(); // returns array
+))->returnRows()); // returns array
                       
 // OR
-$SQLResponce = $si->SELECT("table", ["*", array(
+$SQLResponce = $si->SELECT("table", ["*"], array(
     "id" => 1 // WHERE id=1, instanceof SQLResponce
-)); // returns object      
-$number_of_rows = $SQLResponce->num_rows();
-$rows_as_array = $SQLResponce->returnRows();
+)); // returns object
+
+echo "<br/><br/>- <b>SQL Responce (Object)</b><br/>";
+print_r($SQLResponce);   
+
+echo "<br/><br/><b>Get Number of rows SQLResponce</b><br/>";
+print_r($SQLResponce->num_rows());
+
+echo "<br/><br/><b>Get rows (Array) from SQLResponce</b><br/>";
+print_r($rows_as_array = $SQLResponce->returnRows());
