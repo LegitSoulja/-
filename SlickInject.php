35784416a031d9baca2da5adc635da12bba2bf9d
ᦖєgιт <support@legitsoulja.info>
2017-04-10 21:16:20 -0400

    Update SlickInject.php

--- SlickInject.php
+++ SlickInject.php
@@ -7,7 +7,7 @@ use SlickInject\SQLObject as SQLObject;
 class SlickInject
 {
     protected static $SQLObject;
-	protected static $parser;
+    protected static $parser;
     function connect($dbhost, $dbuser, $dbpass, $dbname)
     {
         self::$SQLObject = new SQLObject();
@@ -15,14 +15,14 @@ class SlickInject
     }
     function isConnected()
     {
-		return (gettype(self::$SQLObject) != null)?true:false;
+        return (gettype(self::$SQLObject) != null) ? true : false;
     }
     static function INSERT($table, $object)
     {
         if (!self::isConnected())
             return new Parser\INSERT($table, $object);
         else
-            return self::$SQLObject->query(new Parser\INSERT($table, $object,self::$SQLObject));
+            return self::$SQLObject->query(new Parser\INSERT($table, $object, self::$SQLObject));
     }
     static function DELETE($table, $object)
     {
@@ -36,17 +36,19 @@ class SlickInject
         if (!self::isConnected())
             return new Parser\SELECT($table, $c, $where);
         else
-            return self::$SQLObject->query(new Parser\SELECT($table, $c, $where,self::$SQLObject));
+            return self::$SQLObject->query(new Parser\SELECT($table, $c, $where, self::$SQLObject));
     }
     static function UPDATE($table, $object, $where)
     {
         if (!self::isConnected())
             return new Parser\UPDATE($table, $object, $where);
         else
-            return self::$SQLObject->query(new Parser\UPDATE($table, $object, $where,self::$SQLObject));
+            return self::$SQLObject->query(new Parser\UPDATE($table, $object, $where, self::$SQLObject));
     }
-	function close(){
-		if (!self::isConnected()) return;
-		return self::$SQLObject->close();
-	}
+    function close()
+    {
+        if (!self::isConnected())
+            return;
+        return self::$SQLObject->close();
+    }
 }
