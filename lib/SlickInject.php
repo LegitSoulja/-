5aff96e2de02c6570925e814c02566666a5f1d0b
ᦖєgιт <support@legitsoulja.info>
2017-04-10 23:09:33 -0400

    Update SlickInject.php

--- lib/SlickInject.php
+++ lib/SlickInject.php
@@ -6,7 +6,7 @@ use SlickInject\SQLObject\SQLObject as SQLObject;
 
 class SlickInject
 {
-    private static $SQLObject;
+    private static $SQLObject = null;
     function connect($dbhost, $dbuser, $dbpass, $dbname)
     {
         self::$SQLObject = new SQLObject();
@@ -14,33 +14,33 @@ class SlickInject
     }
     function isConnected()
     {
-        return (gettype(self::$SQLObject) != null) ? true : false;
+        return (gettype(self::$SQLObject) == "NULL") ? false : true;
     }
     static function INSERT($table, $object)
     {
         if (!self::isConnected())
-            return new Parser\INSERT($table, $object);
+            return (string) new Parser\INSERT($table, $object);
         else
             return self::$SQLObject->query((string) new Parser\INSERT($table, $object, self::$SQLObject), false);
     }
     static function DELETE($table, $object)
     {
         if (!self::isConnected())
-            return new Parser\DELETE($table, $object);
+            return (string) new Parser\DELETE($table, $object);
         else
             return self::$SQLObject->query((string) new Parser\DELETE($table, $object), false);
     }
     static function SELECT($table, $c = null, $where = null)
     {
         if (!self::isConnected())
-            return new Parser\SELECT($table, $c, $where);
+            return (string) new Parser\SELECT($table, $c, $where);
         else
             return self::$SQLObject->query((string) new Parser\SELECT($table, $c, $where), true);
     }
     static function UPDATE($table, $object, $where)
     {
         if (!self::isConnected())
-            return new Parser\UPDATE($table, $object, $where);
+            return (string) new Parser\UPDATE($table, $object, $where);
         else
             return self::$SQLObject->query((string) new Parser\UPDATE($table, $object, $where, self::$SQLObject), false);
     }
