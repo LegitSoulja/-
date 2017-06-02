f447891cc979e797521a20d855d1973e13b7d3bf
ᦖєgιт <support@legitsoulja.info>
2017-06-02 01:01:57 -0400

    Update Parser.php

--- lib/Parser.php
+++ lib/Parser.php
@@ -3,31 +3,26 @@ namespace SlickInject\Parser;
 
 class WHERE
 {
-    private static $data = array();
-    
-    function __construct($a, $sql = null)
-    { self::$data = array($a,$sql); }
-    
-    public function __toString()
+    static function __build($a, $sql = null)
     {
         $z    = "WHERE";
         $type = 0;
-        if (count(self::$data[0]) < 1) return "";
-        foreach (self::$data[0] as $n => $v) {
-            $n = ((!empty(self::$data[1])) ? self::$data[1]->escapeString($n) : mysql_escape_string($n));
-            $v = ((!empty(self::$data[1])) ? self::$data[1]->escapeString($v) : mysql_escape_string($v));
+        if (count($a) < 1) return "";
+        foreach ($a as $n => $v) {
+            $n = (($sql != null) ? $sql->escapeString($n) : mysql_escape_string($n));
+            $v = (($sql != null) ? $sql->escapeString($v) : mysql_escape_string($v));
             if (!empty($n) && !empty($v) && !(is_numeric($n))) {
                 if ($type == 1) $z .= " AND";
                 $type = 1;
-                $z .= " `" . $n . "`=";
+                $z .= " `" . ($n) . "`=";
                 if (is_numeric($v)) {
                     $z .= $v;
                     continue;
                 }
-                $z .= "'$v'";
+                $z .= "'" . ($v) . "'";
             } else {
                 $type = 2;
-                $z .= " " . $v;
+                $z .= " " . ($v);
             }
             continue;
         }
@@ -37,19 +32,14 @@ class WHERE
 
 class INSERT
 {
-    private static $data = array();
-    
-    function __construct($table, $object, $sql = null)
-    {  self::$data = array($table,$object,$sql); }
-    
-    public function __toString()
+   static function __build($table, $object, $sql = null)
     {
-        $z    = "INSERT INTO `" . (self::$data[0]) . "`";
+        $z    = "INSERT INTO `" . ($table) . "`";
         $keys = array(array(),array());
         
-        foreach (self::$data[1] as $n => $v) {
-            $n = ((!empty(self::$data[2])) ? self::$data[2]->escapeString($n) : mysql_escape_string($n));
-            $v = ((!empty(self::$data[2])) ? self::$data[2]->escapeString($v) : mysql_escape_string($v));
+        foreach ($object as $n => $v) {
+            $n = (($sql != null) ? $sql->escapeString($n) : mysql_escape_string($n));
+            $v = (($sql != null) ? $sql->escapeString($v) : mysql_escape_string($v));
             if (!empty($n) && !empty($v) && !(is_numeric($n))) {
                 array_push($keys[0], $n);
                 if (is_numeric($v)) array_push($keys[1], $v);
@@ -60,53 +50,35 @@ class INSERT
     }
 }
 
-class SELECT
+class SELECT 
 {
-    private static $data = array();
-	
-    public function __construct($columns = [], $table, $where = null)
-    {
-        if (!is_array($columns)) throw new \Exception("Args index 0 is not an array.");
-        self::$data = array($columns, $table, $where);
-    }
-	
-    public function __toString()
-    { return "SELECT " . ((empty(self::$data[0])) ? "*" : join(",", self::$data[0])) . " FROM `" . (self::$data[1]) . "` " . (string) ((empty(self::$data[2])) ? null : (new WHERE(self::$data[2]))); }
+    static function __build($columns = [], $table, $where = null, $sql = null)
+    { return "SELECT " . ((count($c) < 1) ? "*" : join(",", $c)) . " FROM `" . ($table) . "` " . (($where != null) ? (WHERE::__build($where, $sql)) : ""); }
 }
 
 class DELETE
 {
-    private static $data = array();
-    
-    public function __construct($table, $object = null)
-    { self::$data = array($table,$object); }
-    
-    public function __toString()
-    { return "DELETE FROM `" . self::$data[0] . "` " . (string) (new WHERE(self::$data[1])); }
+    static function __build($table, $object, $sql = null)
+    { return "DELETE FROM `" . ($table) . "` " .(WHERE::__build($object, $sql)); }
 }
 
 class UPDATE
 {
-    private static $data = array();
-	
-    function __construct($table, $object, $where, $sql = null)
-    { self::$data = array($table,$object,$where,$sql); }
-	
-    function __toString()
+    static function __build($table, $object, $where, $sql = null)
     {
         $z      = "UPDATE %s SET";
-        $where  = (new WHERE(self::$data[2]));
+        $where  = (WHERE::__build($where, $sql));
         $append = "";
-        foreach (self::$data[1] as $n => $v) {
-            $n = ((!empty(self::$data[4])) ? self::$data[4]->escapeString($n) : mysql_escape_string($n));
-            $v = ((!empty(self::$data[4])) ? self::$data[4]->escapeString($v) : mysql_escape_string($v));
+        foreach ($object as $n => $v) {
+            $n = (($sql != null) ? $sql->escapeString($n) : mysql_escape_string($n));
+            $v = (($sql != null) ? $sql->escapeString($v) : mysql_escape_string($v));
             if (!empty($n) && !empty($v) && !is_numeric($n)) {
                 $append .= "`" . $n . "`=";
                 if (is_numeric($v)) $append .= (int) $v . " ";
                 else $append .= "'" . $v . "' ";
             }
         }
-        $z = str_replace("%s", "`" . self::$data[0] . "`", $z);
-        return $z .= " " . $append . $where;
+        $z = str_replace("%s", "`" . ($table) . "`", $z);
+        return $z .= " " . ($append) . ($where);
     }
 }
