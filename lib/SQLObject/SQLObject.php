d69e904052ea875fd0f58caf8595dc74884ec1ff
ᦖєgιт <support@legitsoulja.info>
2017-07-29 22:01:09 -0400

    get

--- lib/SQLObject/SQLObject.php
+++ lib/SQLObject/SQLObject.php
@@ -175,6 +175,7 @@ class SQLObject
      */
     public function query($sql, $bind, $rr = false)
     {
+        if(!$this->isConnected()) throw new \Exception("Can't do it partner. Your database connection is not open.");
         try {
             if ($prep = self::$con->prepare($sql)) {
                 if (isset($bind) && $bind != NULL) {
